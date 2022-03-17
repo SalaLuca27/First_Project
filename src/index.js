954b2ea899120e989916645b9d91e959e1a536e5
SalaLuca27 <salaluca2000@gmail.com>
2022-03-17 12:46:16 +0100

    12.46

--- src/index.js
+++ src/index.js
@@ -7,11 +7,7 @@ import { getMainDefinition } from 'apollo-link-ws/node_modules/apollo-utilities'
 import AppRoutes from './Routes';
 import { Provider } from 'react-redux';
 import {store} from './Store/applicationStore';
-import App from './App';
 import ReactDOM from 'react-dom';
-import Amplify from 'aws-amplify';
-import config from './aws-exports';
-Amplify.configure(config);
 
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
 
@@ -54,13 +50,12 @@ const client = new ApolloClient({
 });
 
 
-function Appp() {
+function App() {
   return (
     <div>
       <Provider store={store}>
           <AppRoutes />
       </Provider>
-      <App />
     </div>
   );
 }
@@ -68,9 +63,10 @@ function Appp() {
 ReactDOM.render(
 
   <ApolloProvider client={client}>
+      <App />
   </ApolloProvider>,
   document.getElementById('root'),
 );
 
 
-export default Appp
+export default App
