70568e56e536e1ec375cfd623e027fcae2d5f49a
SalaLuca27 <salaluca2000@gmail.com>
2022-04-07 11:03:43 +0200

    login

--- src/Components/login.js
+++ src/Components/login.js
@@ -2,7 +2,6 @@ import {useState, useEffect} from 'react';
 import { Link, useNavigate } from "react-router-dom";
 import { ROUTES } from "../Utils/routes";
 import '../css/login.css';
-// import { gql, useMutation } from '@apollo/client';
 import { Auth } from 'aws-amplify';
 
 const LoginUser = () => {    
@@ -11,13 +10,7 @@ const LoginUser = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [token, setToken] = useState("");
-    const [error, setError] = useState(false);
     
-    // const LOGIN = gql`
-    //     mutation Login($username: String!, $password: String!) {
-    //         login(username: $username, password: $password)
-    //     }`;
-
     const handleChange = (event) => {
         if(event.target.name === 'username') {
             setUsername(event.target.value);
@@ -26,21 +19,20 @@ const LoginUser = () => {
         }
     }
 
-    // const [login, {loading}] = useMutation(LOGIN, {variables : {'username' : username, 'password' : password}});
-
     const handleClick = async(event) => {
         if(username === '' || password === ''){
-            setError(true);
+            alert('Compilare tutti i campi');
         }else{
             event.preventDefault();
-            const user = Auth.signIn(username, password);
-            user
-            .then((data) => setToken(data.signInUserSession.accessToken.jwtToken))
-            .catch((error) => {if(error){setError(true)}})
-        }
-
-        if(!error){
-            alert('Verificare che tutti i campi inseriti siano corretti')
+            await Auth.signIn(username, password)
+                .then((data) => {
+                    setToken(data.signInUserSession.accessToken.jwtToken);
+                })
+                .catch((error) => {
+                    if(error){
+                        alert('Username o password errati');
+                    }
+                })
         }
     }
 
