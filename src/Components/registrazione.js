f4cbe65dc10e4a9203b5ce1d3bce2fcabf7140c8
SalaLuca27 <salaluca2000@gmail.com>
2022-04-07 12:56:42 +0200

    uploda registrazione

--- src/Components/registrazione.js
+++ src/Components/registrazione.js
@@ -8,14 +8,12 @@ import { Auth } from "aws-amplify";
 const Registrazione = () => {
 
     const navigate = useNavigate();
-    const [pswError, setPswError] = useState(false);
     const [email, setEmail] = useState("");
     const [verifyPassword, setVerifyPassword] = useState("");
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [openForm, setOpenForm] = useState(true);
     const [code, setCode] = useState("");
-    const [lenghtError, setLengthError] = useState(false);
 
     const handleChange = (event) => {
         if(event.target.name === 'username'){
@@ -37,47 +35,52 @@ const Registrazione = () => {
 
     const handleClick = (event) => {
         event.preventDefault();
+        if(username === '' || password === '' || verifyPassword === '' || email === ''){
+            throw alert('Compile all the fields');
+        }
         if(password.length >= 8){
             if(password === verifyPassword){
                 const user = Auth.signUp(username, password, email);
                 console.log('user:', user);
                 user
-                    .then(data => {
+                    .then((data) => {
+                        console.log('data:', data);
                         setOpenForm(false);
                     })
-                    .catch(error => {
+                    .catch((error) => {
+                        console.log('error:', error);
                         if(error.message === 'User already exists'){
                             alert('Username già utilizzato');
+                        }
+                        else if(error.message === 'Invalid email address format.'){
+                            alert('Email not valid');
                         }
                     })
             }
             else{
-                setPswError(true);
-                console.log('Password no match')
+                alert('Password is not the same')
             }
         }else{
-            setLengthError(true);
+            alert('Password must be composed by 8 characters');
         }
     }
 
     const confirm = () => {
-        if(code === ''){
-            alert('OTP can not be null');
-            navigate(ROUTES.registrazione);
-            setOpenForm(false);
+        if(code.length !== 6 || code === ''){
+            throw alert('Incorrect code');
         }
-        else{
-            const user = Auth.confirmSignUp(username, code);
-            user
-                .then((data)=> {
-                    console.log('data:', data)
-                    navigate(ROUTES.login);
-                })
-                .catch((error)=> {
-                    console.log('error:', error);
-                    alert('OTP code not correct');
-                })
-        }
+
+        const user = Auth.confirmSignUp(username, code);
+        user
+            .then((data)=> {
+                console.log('dataConfirm:', data)
+                navigate(ROUTES.login);
+            })
+            .catch((error)=> {
+                console.log('errorConfirm:', error);
+                alert('OTP code not correct');
+            })
+        
     }
 
     return (
@@ -86,25 +89,21 @@ const Registrazione = () => {
             {openForm ? 
 
                 <form>
-                    {lenghtError ? 'Lunghezza minima password 8 caratteri' : ''}
-                    <br/>
-                    {pswError ? 'Password is not the same' : ''}
-                    <br/>
                     <div className="mb-3">
                         <label className="form-label">Username</label>
-                        <input type="text" className="form-control" placeholder="username" name="username" onChange={handleChange}/>
+                        <input type="text" className="form-control" placeholder="username" name="username" onChange={handleChange} required/>
                     </div>
                     <div className="mb-3">
                         <label className="form-label">Password</label>
-                        <input type="password" className="form-control" id="password" placeholder="password" minLength={8} name="password" onChange={handleChange}/>
+                        <input type="password" className="form-control" id="password" placeholder="password" minLength={8} name="password" onChange={handleChange} required/>
                     </div>
                     <div className="mb-3">
                         <label className="form-label">Confirm Password</label>
-                        <input type="password" className="form-control" id="verifyPassword" placeholder="password" minLength={8} name="verifyPassword" onChange={handleChange}/>
+                        <input type="password" className="form-control" id="verifyPassword" placeholder="password" minLength={8} name="verifyPassword" onChange={handleChange} required/>
                     </div>
                     <div className="mb-3">
                         <label className="form-label">Email</label>
-                        <input type="email" className="form-control" placeholder="email@email.com" name="email" onChange={handleChange}/>
+                        <input type="email" className="form-control" placeholder="email@email.com" name="email" onChange={handleChange} required/>
                     </div>
                     <div className = "buttonContainer">
                         <button className="btn btn-primary" onClick={handleClick}>Registrati</button>
@@ -124,10 +123,10 @@ const Registrazione = () => {
                 <form>
                     <div className="mb-3">
                         <label className="form-label">Inserici codice OTP</label>
-                        <input type="text" className="form-control" name="code" onChange={(event) => setCode(event.target.value)} />
+                        <input type="text" className="form-control" name="code" onChange={(event)=>setCode(event.target.value)} />
                     </div>
                     <div className = "buttonContainer">
-                        <button className="btn btn-primary" onClick={confirm}>Conferma codice</button>
+                        <button id= "button" className="btn btn-primary" onClick={confirm}>Conferma codice</button>
                     </div>
                 </form>
 
